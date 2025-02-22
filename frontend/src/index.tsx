68eafde62dcf1bc321a7e443229897b40b96e72f
Jackson Jacobson <cjacobsonnn@gmail.com>
2025-02-21 18:35:04 -0700

    Updated to get npm to start successfully

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import App from './App';
 
-// For React 17, use ReactDOM.render:
-ReactDOM.render(
+const container = document.getElementById('root');
+if (!container) {
+  throw new Error("Failed to find the root element");
+}
+const root = ReactDOM.createRoot(container);
+
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
