84714aa059fee1a5db4ab80052159b3df8596194
Jackson Jacobson <cjacobsonnn@gmail.com>
2025-02-24 22:51:56 -0700

    Frontend Modernization V1.2

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import mapboxgl from 'mapbox-gl';
+import 'mapbox-gl/dist/mapbox-gl.css';
 import Navigation from './components/Navigation';
 import HeroSection from './components/HeroSection';
 import MapSection from './components/MapSection';
 import FeaturesSection from './components/FeaturesSection';
-import Footer from './components/Footer'; // Assuming you'll create this
+import Footer from './components/Footer';
+
+mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_ACCESS_TOKEN || '';
 
 const App: React.FC = () => {
   return (
---
+++ frontend/src/types.d.ts
@@ -0,0 +1,2 @@
+declare module '@headlessui/react';
+declare module '@heroicons/react/24/outline';
